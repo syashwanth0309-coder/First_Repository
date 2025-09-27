152b1295798f86b99e2b2e96b6b45385f47c7d4d
Yashwanth <s.yashwanth0309@gmail.com>
2025-09-27 07:43:03 +0530

    Initial commit of Django calculator

---
+++ calculator/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    result = None
+    if request.method == 'POST':
+        num1 = float(request.POST['num1'])
+        num2 = float(request.POST['num2'])
+        operation = request.POST['operation']
+        if operation == 'add':
+            result = num1+num2
+        elif operation == 'subtract':
+            result = num1-num2
+        elif operation == 'multiply':
+            result = num1*num2
+        elif operation == 'divide':
+            if num2 != 0:
+                result = num1/num2
+            else:
+                result = "Error: Division by zero"
+    return render(request, 'calculator/home.html', {'result': result})
